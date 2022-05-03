58cffc90608472ffba2c21fe7b1422c07007c223
Luka <kazicluka4@gmail.com>
2022-05-03 19:23:53 +0200

    Fixed a bug with modal window

--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,21 @@
 <div>
 <NavBar></NavBar>
 <AllProducts></AllProducts>
+<ShoppingCartModal></ShoppingCartModal>
 </div>
 </template>
 
 <script>
+import ShoppingCartModal from '@/components/Modal/ShoppingCartModal.vue';
 import NavBar from '@/components/navbar/NavBar';
 import AllProducts from '@/components/ProductList/AllProducts.vue';
-
 
 export default {
   name: 'IndexPage',
   components: {
     NavBar,
     AllProducts,
+    ShoppingCartModal,
 },
   data() {
     return {
--- store/index.js
+++ store/index.js
@@ -140,5 +140,4 @@ export const state = () => ({
      }
   };
   export const getters = {
-
 }
